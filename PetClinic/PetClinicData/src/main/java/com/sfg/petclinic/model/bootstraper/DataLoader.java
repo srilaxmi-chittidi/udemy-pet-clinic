37691e70cc535a807bb49e6887f81512ab64b1de
laxmi.chittidi <laxmi.chittidi@gmail.com>
2023-08-24 12:54:50 -0700

    Adding PetTypes in BootStrap

--- PetClinic/PetClinicData/src/main/java/com/sfg/petclinic/model/bootstraper/DataLoader.java
+++ PetClinic/PetClinicData/src/main/java/com/sfg/petclinic/model/bootstraper/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.sfg.petclinic.model.Owner;
+import com.sfg.petclinic.model.PetType;
 import com.sfg.petclinic.model.Vet;
 import com.sfg.petclinic.services.OwnerService;
+import com.sfg.petclinic.services.PetTypeService;
 import com.sfg.petclinic.services.VetService;
 
 @Component
@@ -13,14 +15,24 @@ public class DataLoader implements CommandLineRunner {
 	
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 	
-	public DataLoader(OwnerService ownerService,VetService vetService) {
+	public DataLoader(OwnerService ownerService,VetService vetService,PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService   = vetService;
+		this.petTypeService  = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		petTypeService.save(cat);
 		
 		Owner owner1 = new Owner();
 		owner1.setId(1L);
