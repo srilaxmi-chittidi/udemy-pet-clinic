4ca60c50d8fd2e8ddc01899a6994b4233870d87d
laxmi.chittidi <laxmi.chittidi@gmail.com>
2023-08-25 10:41:12 -0700

    Enhance Owners with Contact Info and Pets on start up #8

--- PetClinic/PetClinicData/src/main/java/com/sfg/petclinic/services/map/OwnerServiceMap.java
+++ PetClinic/PetClinicData/src/main/java/com/sfg/petclinic/services/map/OwnerServiceMap.java
@@ -6,14 +6,36 @@ import org.springframework.stereotype.Service;
 
 import com.sfg.petclinic.model.Owner;
 import com.sfg.petclinic.services.OwnerService;
+import com.sfg.petclinic.services.PetService;
+import com.sfg.petclinic.services.PetTypeService;
 
 @Service
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> 
+public class OwnerServiceMap  extends AbstractMapService<Owner, Long> 
 							 implements OwnerService {
 
+	private final PetTypeService petTypeService;
+	private final PetService petService;
+	 
+	public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+	        this.petTypeService = petTypeService;
+	        this.petService = petService;
+	}
 	@Override
 	public Owner save(Owner object) {
-		return super.save(object);
+		if(object != null) {
+			if(object.getPets() != null) {
+				object.getPets().forEach(pet-> {
+						if(pet.getPetType() != null){
+							if(pet.getPetType().getId() != null) {
+								pet.setPetType(petTypeService.save(pet.getPetType()));
+							}
+						}
+				});
+			}
+			return super.save(object);
+		}else {
+			return null;
+		}
 	}
 
 	@Override
@@ -43,6 +65,8 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long>
                 .findFirst()
                 .orElse(null);
 	}
+	
+	
 
 	
 }
